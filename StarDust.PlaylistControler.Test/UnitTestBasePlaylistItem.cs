3225dd6a3cb5e943ad7e4cbf041a2bbcc72e0c1c
Romain Jourde <dust63@hotmail.fr>
2019-03-03 20:49:01 +0100

    Add mixed relation test. And more assert

--- StarDust.PlaylistControler.Test/UnitTestBasePlaylistItem.cs
+++ StarDust.PlaylistControler.Test/UnitTestBasePlaylistItem.cs
@@ -16,6 +16,7 @@ namespace StarDust.PlaylistControler.Test
             element1.ChildElement = element2;
             element2.ChildElement = element3;
 
+            Assert.Null(element1.ParentElement);
             Assert.StrictEqual(element1.ChildElement, element2);
             Assert.StrictEqual(element2.ParentElement, element1);
 
@@ -28,7 +29,7 @@ namespace StarDust.PlaylistControler.Test
             Assert.StrictEqual(element2.ChildElement, element3);
             Assert.StrictEqual(element3.ParentElement, element2);
 
-
+            Assert.Null(element3.ChildElement);
         }
 
         [Fact]
@@ -42,12 +43,36 @@ namespace StarDust.PlaylistControler.Test
             element2.ParentElement = element1;
             element3.ParentElement = element2;
 
+            Assert.Null(element1.ParentElement);
             Assert.StrictEqual(element1.ChildElement, element2);
             Assert.StrictEqual(element2.ParentElement, element1);
 
             element2.ParentElement = null;
             Assert.Null(element2.ParentElement);
             Assert.Null(element1.ChildElement);
+
+            Assert.StrictEqual(element2.ChildElement, element3);
+            Assert.StrictEqual(element3.ParentElement, element2);
+
+            Assert.Null(element3.ChildElement);
+        }
+
+
+        [Fact]
+        public void TestMixedRelation()
+        {
+
+            var element1 = new TestPlaylistElement { ID = 1 };
+            var element2 = new TestPlaylistElement { ID = 2 };
+            var element3 = new TestPlaylistElement { ID = 3 };
+
+
+            element3.ParentElement = element2;
+            element1.ChildElement = element2;
+
+
+            Assert.StrictEqual(element1.ChildElement, element2);
+            Assert.StrictEqual(element2.ParentElement, element1);
 
             Assert.StrictEqual(element2.ChildElement, element3);
             Assert.StrictEqual(element3.ParentElement, element2);
